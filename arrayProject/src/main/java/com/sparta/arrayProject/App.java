1ef0c7ee6ec274b2c3e1114dd4e997c0af80e07f
unloosedcascade <matthewdan.handley@btinternet.com>
2022-03-09 09:16:51 +0000

    Started the merge array part of the project, have initialised the final array but need to add the input arrays to the final array and sort through them

--- arrayProject/src/main/java/com/sparta/arrayProject/App.java
+++ arrayProject/src/main/java/com/sparta/arrayProject/App.java
@@ -5,7 +5,12 @@ public class App {
 
     public static void main(String[] args) {
         int[] myArray = new int[]{14, 56, 23, 18, 100, 91, 84};
+        int[] array1 = {1, 15, 23, 9, 5, 18};
+        int[] array2 = {77, 18, 2, 19, 5, 3};
         BubbleSort.bubbleSort(myArray);
+        BubbleSort.bubbleSort(array1);
+        BubbleSort.bubbleSort(array2);
         System.out.println(Arrays.toString(myArray));
+        System.out.println(MergeSort.mergeSort(array1, array2));
         }
     }
---
+++ arrayProject/src/main/java/com/sparta/arrayProject/MergeSort.java
@@ -0,0 +1,8 @@
+package com.sparta.arrayProject;
+
+public class MergeSort {
+    public static int[] mergeSort(int[] arrayIn1, int [] arrayIn2){
+        int[] mergedArrays = new int[arrayIn1.length + arrayIn2.length];
+
+    }
+}
