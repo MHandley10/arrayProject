6e820f21c792ccbdc78cad592af6f22af9cb51fc
unloosedcascade <matthewdan.handley@btinternet.com>
2022-03-09 09:20:45 +0000

    Changed the final variables name to better suit it's function and added a return to the class

--- arrayProject/src/main/java/com/sparta/arrayProject/MergeSort.java
+++ arrayProject/src/main/java/com/sparta/arrayProject/MergeSort.java
@@ -2,7 +2,7 @@ package com.sparta.arrayProject;
 
 public class MergeSort {
     public static int[] mergeSort(int[] arrayIn1, int [] arrayIn2){
-        int[] mergedArrays = new int[arrayIn1.length + arrayIn2.length];
+        int[] mergedArray = new int[arrayIn1.length + arrayIn2.length];
 
-    }
+    return mergedArray;}
 }
