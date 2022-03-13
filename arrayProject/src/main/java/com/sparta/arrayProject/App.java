aeb49337296d4c9ddc9f4cfbb69649c2f5232d89
unloosedcascade <matthewdan.handley@btinternet.com>
2022-03-13 20:44:55 +0000

    Started work on the Merge Sort algorithm, more to complete tomorrow

--- arrayProject/src/main/java/com/sparta/arrayProject/App.java
+++ arrayProject/src/main/java/com/sparta/arrayProject/App.java
@@ -7,6 +7,7 @@ public class App {
         int[] myArray = new int[]{14, 56, 23, 18, 100, 91, 84};
         int[] array1 = {1, 15, 23, 9, 5, 18};
         int[] array2 = {77, 18, 2, 19, 5, 3};
+        int[] mergeSortArray = {85, 19, 300, 21, 500, 45, 1, 300};
         BubbleSort.bubbleSort(myArray);
         BubbleSort.bubbleSort(array1);
         BubbleSort.bubbleSort(array2);
---
+++ arrayProject/src/main/java/com/sparta/arrayProject/SplitMergeSort.java
@@ -0,0 +1,16 @@
+package com.sparta.arrayProject;
+
+public class SplitMergeSort {
+    public static int[] splitMergeSort(int[] arrayIn){
+        int midPoint = arrayIn.length / 2;
+        int[] splitArray1 = new int[midPoint];
+        int[] splitArray2 = new int[midPoint];
+        if (arrayIn.length == 1){
+            return arrayIn;
+        } else if (arrayIn.length > 1){
+            for (){
+
+            }
+        }
+    return splitMergeSort(splitArray1), splitMergeSort(splitArray2)}
+}
